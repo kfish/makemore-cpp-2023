ff24c383e975dd952c6360308843429a59620f32
Conrad Parker <conrad@metadecks.org>
2023-10-21 13:22:45 +1100

    matplotlib-example: add save option

--- examples/matplotlib-example.cpp
+++ examples/matplotlib-example.cpp
@@ -1,8 +1,34 @@
+#include <iostream>
+#include <unistd.h> // for getopt
+
 #include "matplotlibcpp.h"
 
 namespace plt = matplotlibcpp;
 
-int main() {
-    plt::plot({1,3,2,4});
-    plt::show();
+int main(int argc, char *argv[]) {
+    int opt;
+    std::string output_file;
+
+    while ((opt = getopt(argc, argv, "o:")) != -1) {
+        switch (opt) {
+        case 'o':
+            output_file = std::string(optarg);
+            break;
+        default:
+            std::cerr << "Usage: " << argv[0] << " [-o output_file]" << std::endl;
+            return 1;
+        }
+    }
+
+    std::vector<double> y = {1.0, 2.9, 3.8};
+
+    plt::plot(y);
+
+    if (!output_file.empty()) {
+        plt::save(output_file);
+    } else {
+        plt::show();
+    }
+
+    return 0;
 }
