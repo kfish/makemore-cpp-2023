500a2cbea3ceaeb75ae158073a9a665f0647e2fb
Conrad Parker <conrad@metadecks.org>
2023-10-22 08:57:01 +1100

    bigram: use Eigen directly

--- examples/bigram.cpp
+++ examples/bigram.cpp
@@ -1,42 +1,51 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <Eigen/Dense>
 #include <cctype>
+#include "matplotlibcpp.h"  // Make sure this is the version from the Cryoris fork
 
-Eigen::MatrixXi generate_bigram_distribution(const std::string& filename) {
-    Eigen::MatrixXi bigram_freq = Eigen::MatrixXi::Zero(27, 27);  // Initialize 27x27 matrix with zeros
+namespace plt = matplotlibcpp;
+
+Eigen::MatrixXd generate_bigram_distribution(const std::string& filename) {
+    Eigen::MatrixXd bigram_freq = Eigen::MatrixXd::Zero(27, 27);
     std::ifstream file(filename);
-
     if (!file) {
         std::cerr << "Failed to open file." << std::endl;
         return bigram_freq;
     }
-
     std::string word;
     while (file >> word) {
-        int prev_index = 0;  // Using 0 for '.'
+        int prev_index = 0;
         for (char c : word) {
             c = std::tolower(c);
-            if (c < 'a' || c > 'z') continue;  // Skip non-alphabetical characters
-
-            int curr_index = c - 'a' + 1;  // 1-26 for a-z
+            if (c < 'a' || c > 'z') continue;
+            int curr_index = c - 'a' + 1;
             ++bigram_freq(prev_index, curr_index);
             prev_index = curr_index;
         }
         if (prev_index != 0) {
-            ++bigram_freq(prev_index, 0);  // Increment frequency for the end token
+            ++bigram_freq(prev_index, 0);
         }
     }
-
     return bigram_freq;
 }
 
 int main(int argc, char *argv[]) {
     const std::string filename = argv[1];
-    Eigen::MatrixXi bigram_freq = generate_bigram_distribution(filename);
 
-    std::cout << bigram_freq << std::endl;
+    // Generate the bigram_freq matrix
+    Eigen::MatrixXd bigram_freq = generate_bigram_distribution(filename);
+
+    // Flattening the Eigen::MatrixXd to Eigen::VectorXd using Eigen::Map
+    //Eigen::Map<Eigen::VectorXd> data(bigram_freq.data(), bigram_freq.size());
+
+    // Plotting
+    plt::imshow(bigram_freq, {{"cmap", "Blues"}});
+    plt::title("Bigram Frequency Distribution");
+    plt::xlabel("Next Character (Index)");
+    plt::ylabel("Current Character (Index)");
+    plt::colorbar();
+    plt::show();
 
     return 0;
 }
