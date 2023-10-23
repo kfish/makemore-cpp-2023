a7d8bee2ce7d11dc5a377b8861af0afc87297c38
Conrad Parker <conrad@metadecks.org>
2023-10-24 09:43:19 +1100

    multinomial.h: add header

---
+++ include/multinomial.h
@@ -0,0 +1,33 @@
+#include <random>
+#include <Eigen/Dense>
+#include <vector>
+
+class MultinomialSampler {
+private:
+    std::vector<std::discrete_distribution<int>> distributions; // Cached distributions
+    std::mt19937 rng; // Random number generator
+
+    // Cache discrete_distribution instances for each row of the probability matrix
+    void cache_distributions(const Eigen::MatrixXd& prob_matrix) {
+        for (int i = 0; i < prob_matrix.rows(); ++i) {
+            std::discrete_distribution<int> dist(prob_matrix.row(i).data(), prob_matrix.row(i).data() + prob_matrix.cols());
+            distributions.push_back(dist);
+        }
+    }
+
+public:
+    // Constructor that takes a precalculated probability matrix
+    MultinomialSampler(const Eigen::MatrixXd& prob_matrix) : rng(std::random_device{}()) {
+        cache_distributions(prob_matrix);
+    }
+
+    // Operator to sample given a row index
+    size_t operator()(size_t row_idx) {
+        if (row_idx >= distributions.size()) {
+            throw std::out_of_range("Row index out of bounds");
+        }
+
+        // Use the pre-cached distribution to sample and return column index
+        return distributions[row_idx](rng);
+    }
+};
